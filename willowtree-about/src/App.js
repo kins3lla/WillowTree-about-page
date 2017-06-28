8425410985695f21ddca2656e07107e5529e8ecd
nayanika <nayanika@nayanikas-MacBook-Air.local>
2017-06-28 08:35:31 -0400

    adding division 3

--- willowtree-about/src/App.js
+++ willowtree-about/src/App.js
@@ -3,6 +3,10 @@ import willowtree from './willowtree.png';
 import './App.css';
 import Header from './Header.js'
 import Division1 from './Division1.js'
+import Division2 from './Division2.js'
+import Division3 from './Division3.js'
+import Division4 from './Division4.js'
+
 
 class App extends React.Component {
 
@@ -11,6 +15,8 @@ class App extends React.Component {
       <div className="App">
 
         <Division1 />
+        <Division2 />
+        <Division4 />
 
 
       </div>
---
+++ willowtree-about/src/Division3.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import willowtree from './willowtree.png';
+import './division2.css';
+import Header from './Header.js'
+
+import { Card, Col, Row } from 'antd';
+
+
